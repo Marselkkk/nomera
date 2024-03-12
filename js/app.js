b3c1d3e029b3944c7346e6905c5f01304208c781
marcele <marsgyl@gmail.com>
2024-03-12 08:01:39 +0300

    upd

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import { accordion } from "./modules/accordion.js";
 import { burger } from "./modules/burger.js";
 import { slider } from "./modules/slider.js";
-import { yakor } from "./modules/yakor.js";
+import { anchor } from "./modules/anchor.js";
 import { faq } from "./modules/faq.js";
 import { sticky } from "./modules/sticky.js";
 import { validation } from "./modules/validation.js";
@@ -11,7 +11,7 @@ scroll()
 validation()
 sticky()
 faq()
-yakor()
+anchor()
 slider()
 burger()
 accordion()
--- js/modules/anchor.js
+++ js/modules/anchor.js
@@ -1,4 +1,4 @@
-export function yakor() {
+export function anchor() {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
